89c0718ca33492bc0d801f2f6e64cb16b82b2137
Dileep Guntamadugu <dileep.guntamadugu@gmail.com>
2021-07-22 20:45:52 +1000

    Initial commit

---
+++ src/main/java/com/example/accessingmongodbdatarest/Person.java
@@ -0,0 +1,80 @@
+package com.example.accessingmongodbdatarest;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+	@Id private String id;
+
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private String mobileNumber;
+	private String driversLicense;
+	private String passport;
+	private String dateOfBirth;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getDriversLicense() {
+		return driversLicense;
+	}
+
+	public void setDriversLicense(String driversLicense) {
+		this.driversLicense = driversLicense;
+	}
+
+	public String getPassport() {
+		return passport;
+	}
+
+	public void setPassport(String passport) {
+		this.passport = passport;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+}
